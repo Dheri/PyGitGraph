00aa68653fa9b08d4b373509196f154aaa073005
Parteek Dheri <ps.dheri@gmail.com>
2020-07-05 00:14:32 -0400

    added weights to edges

--- src/PyGitGraph/utils/createGraph.py
+++ src/PyGitGraph/utils/createGraph.py
@@ -12,33 +12,45 @@ def create_graph():
     head_commit = repo.head.commit
     graph = nx.DiGraph()
 
-    add_parent_chain_to_graph(graph, head_commit)
-    print(list(graph.successors("7392f2d90373013dcd7c08b3db18379638680f7e")))
-    pos = nx.spiral_layout(graph)
+    # print(repo.references)
+    for ref in repo.references:
+        print(ref.commit, ref)
+        add_parent_chain_to_graph(graph, ref.commit)
+
+    # add_parent_chain_to_graph(graph, head_commit)
+    print('graph.nodes list', list(graph.nodes))
+    # for node in graph.nodes:
+    #     print(node)
+    # print(list(graph.successors("7392f2d90373013dcd7c08b3db18379638680f7e")))
+    pos = nx.spring_layout(graph, weight='weight')
     options = {
-        'node_color': 'blue',
-        'node_size': 12,
+        'node_color': 'orange',
+        'node_size': 2,
         'width': 1,
         'arrow_size': 3,
         'with_labels': False,
         'pos': pos
     }
+    print('number_of_nodes', graph.number_of_nodes())
+    print(list(graph.edges(data=True)))
 
-    nx.draw_networkx(graph,  **options)
+    nx.draw_networkx(graph, **options)
     nx.write_gexf(graph, 'output.gexf')
     plt.show()
 
 
 def add_parent_chain_to_graph(graph, rev):
     print('graph len is', len(graph), 'rev is ', rev.hexsha)
-    print('short_sha', rev.hexsha[:5])
-    print('committed_date', rev.committed_date)
+    print('short_sha', rev.hexsha, 'committed_date', rev.committed_date)
 
     for parent in rev.parents:
         if graph.has_edge(rev.hexsha, parent.hexsha):
             print('edge already in graph')
             return
-        graph.add_edge(rev.hexsha, parent.hexsha)
+        # weight = (len(graph) % 24) + 1
+        weight = 1
+        print('weight: ', weight)
+        graph.add_edge(rev.hexsha, parent.hexsha, weight=weight)
         add_parent_chain_to_graph(graph, parent)
 
 
